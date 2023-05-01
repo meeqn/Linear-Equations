35e386dea9c92837136d5c8db353acfb2fea2b08
miqn <mik.bra@wp.pl>
2023-05-01 19:35:08 +0200

    up

--- Matrix.py
+++ Matrix.py
@@ -1,14 +1,33 @@
+import math
+
+
 class Matrix:
+
+    def size_m(self):
+        return self.dimensions()[0]
+
+    def size_n(self):
+        return self.dimensions()[1]
 
     def dimensions(self):
         return len(self.matrix_rows), len(self.matrix_rows[0])
+
+    def calculate_vector_norm(self):
+        if self.dimensions()[1] == 1:
+            norm = 0
+            for element in self.matrix_rows:
+                norm += element[0] ** 2
+            return math.sqrt(norm)
+        else:
+            print("Matrix is not a vector!")
+            exit()
 
     def __add__(self, second_matrix):
         if self.dimensions() == second_matrix.dimensions():
             tmp_mat = Matrix([])
             tmp_rows = self.matrix_rows
-            for M in range(len(tmp_rows)):
-                for N in range(len(tmp_rows[0])):
+            for M in range(0, self.size_m()):
+                for N in range(0, self.size_n()):
                     tmp_rows[M][N] += second_matrix.matrix_rows[M][N]
             tmp_mat.matrix_rows = tmp_rows
             return tmp_mat
@@ -20,8 +39,8 @@ class Matrix:
         if self.dimensions() == second_matrix.dimensions():
             tmp_mat = Matrix([])
             tmp_rows = self.matrix_rows
-            for M in range(len(tmp_rows)):
-                for N in range(len(tmp_rows[0])):
+            for M in range(0, self.size_m()):
+                for N in range(0, self.size_n()):
                     tmp_rows[M][N] -= second_matrix.matrix_rows[M][N]
             tmp_mat.matrix_rows = tmp_rows
             return tmp_mat
@@ -29,14 +48,43 @@ class Matrix:
             print("Matrix dimensions don't match!")
             exit()
 
-    # def __mul__(self, second_matrix):
-    #     if self.dimensions()[0] == second_matrix.dimensions()[1]:
-    #
-    #     else:
-    #         print("matrix dimensions don't match!")
-    #         return Matrix([])
+    def to_string(self):
+        text = ""
+        for row in self.matrix_rows:
+            for element in row:
+                text += str(element)+" "
+            text += "\n"
+        return text
+
+    def __mul__(self, second_matrix):
+        if self.size_n() == second_matrix.size_m():
+            mat = Matrix().uniform_matrix(self.size_m(), second_matrix.size_n(), 0)
+            for rows1 in range(0, self.size_m()):
+                for columns2 in range(0, second_matrix.size_n()):
+                    dot_prod = 0
+                    for i in range(0, self.size_n()):
+                        dot_prod += self.matrix_rows[rows1][i] * second_matrix.matrix_rows[i][columns2]
+                    mat.matrix_rows[rows1][columns2] = dot_prod
+            return mat
+
+        else:
+            print("Matrix dimensions don't match!")
+            exit()
 
     def __init__(self, *rows):
         self.matrix_rows = []
         for row in rows:
             self.matrix_rows.append(row)
+
+    @staticmethod
+    def uniform_matrix(m, n, fill):
+        mat = Matrix()
+        for i_m in range(0, m):
+            row = [fill] * n
+            mat.matrix_rows.append(row)
+        return mat
+
+    @staticmethod
+    def vector(n, fill):
+        mat = Matrix().uniform_matrix(n, 1, fill)
+        return mat
--- main.py
+++ main.py
@@ -1,8 +1,78 @@
 from Matrix import *
+import math
+
+
+def create_A_matrix(N, a1, a2, a3):
+    A = Matrix().uniform_matrix(N, N, 0)
+    for i in range(0, N):
+        for j in range(0, N):
+            if i == j:
+                A.matrix_rows[i][j] = a1
+                if i > 0:
+                    A.matrix_rows[i-1][j] = a2
+                if i < N - 1:
+                    A.matrix_rows[i+1][j] = a2
+                if i > 1:
+                    A.matrix_rows[i - 2][j] = a3
+                if i < N - 2:
+                    A.matrix_rows[i + 2][j] = a3
+    return A
+
+
+def Jacobi(A, b, x, res_error):
+    x_new = Matrix().vector(x.size_m(), 0)
+    MAX_JACOBI_ITER = 1000
+
+    for r in range(MAX_JACOBI_ITER):
+        for i in range(x.size_m()):
+            iter_sum = 0
+            for j in range(A.size_n()):
+                if j != i:
+                    iter_sum += x.matrix_rows[j][0] * A.matrix_rows[i][j]
+
+            x_new.matrix_rows[i][0] = (b.matrix_rows[i][0] - iter_sum) / A.matrix_rows[i][i]
+        x = x_new
+        res = A * x - b
+        if res.calculate_vector_norm() <= res_error:
+            print(r)
+            return x
+
+
+def Gauss_Seidler(A, b, x, res_error):
+    x_new = Matrix().vector(x.size_m(), 0)
+    MAX_GAUSS_ITER = 1000
+
+    for r in range(MAX_GAUSS_ITER):
+        for i in range(x.size_m()):
+            iter_sum = 0
+            for j in range(A.size_n()):
+
+                if j < i:
+                    iter_sum += x_new.matrix_rows[j][0] * A.matrix_rows[i][j]
+                if j > i:
+                    iter_sum += x.matrix_rows[j][0] * A.matrix_rows[i][j]
+
+            x_new.matrix_rows[i][0] = (b.matrix_rows[i][0] - iter_sum) / A.matrix_rows[i][i]
+        x = x_new
+        res = A * x - b
+        if res.calculate_vector_norm() <= res_error:
+            print(r)
+            return x
+
+def zadA(f, e, c, d):
+    a1 = 5 + e
+    a2 = -1
+    a3 = a2
+    N = 900 + 10 * c + d
+    A = create_A_matrix(N, a1, a2, a3)
+    b = Matrix().vector(N, 0)
+    for n in range(0, N):
+        b.matrix_rows[n][0] = math.sin(n * (f + 1))
+
+    x = Matrix().vector(N, 0)
+    res_error = 10 ** -9
+    x_jacobi = Jacobi(A, b, x, res_error)
+    x_gauss = Gauss_Seidler(A, b, x, res_error)
 
 if __name__ == '__main__':
-    mat = Matrix([1, 2, 3], [4, 5, 6])
-    mat2 = Matrix([1, 2, 3])
-    mat3 = mat + mat2
-    print(mat3)
-    print("hi")
+    zadA(8, 8, 1, 3)
